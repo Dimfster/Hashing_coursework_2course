44a3c83e679ef0079dd501cc198c52fa1b68de95
Dimfster <140328074+Dimfster@users.noreply.github.com>
2023-11-11 14:08:16 +0300

    Update Heshirovanie_main.cpp

--- Heshirovanie_main/Heshirovanie_main.cpp
+++ Heshirovanie_main/Heshirovanie_main.cpp
@@ -1,10 +1,13 @@
 ﻿// Heshirovanie_main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+
+
+/*
+* 
+*/
 
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "aeztgstf";
 }
