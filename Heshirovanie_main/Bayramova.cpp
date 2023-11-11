45a0162547257cb2f3dbd6032a07b3c71e53f2a5
Dimfster <140328074+Dimfster@users.noreply.github.com>
2023-11-11 16:03:28 +0300

    Очистка

--- Heshirovanie_main/Bayramova.cpp
+++ Heshirovanie_main/Bayramova.cpp
@@ -1,9 +1,6 @@
-// Heshirovanie_main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
 #include <iostream>
 
-int main()
+int Print()
 {
     std::cout << "Hello World!\n";
 }
--- Heshirovanie_main/Heshirovanie_main.cpp
+++ Heshirovanie_main/Heshirovanie_main.cpp
@@ -1,13 +1,11 @@
 ﻿// Heshirovanie_main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
 
-/*
-* 
-*/
 
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    Print();
 }
