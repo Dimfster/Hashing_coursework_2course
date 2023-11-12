10c3b750c12204e4e81b39b2852d0a100368e651
Dimfster <140328074+Dimfster@users.noreply.github.com>
2023-11-12 22:33:08 +0300

    Update Heshirovanie_main.cpp

--- Heshirovanie_main/Heshirovanie_main.cpp
+++ Heshirovanie_main/Heshirovanie_main.cpp
@@ -1,11 +1,13 @@
 ﻿// Heshirovanie_main.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 
 
+// Слово хешируем слово "Bitcoin";
+
 
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
-    Print();
+    
 }
