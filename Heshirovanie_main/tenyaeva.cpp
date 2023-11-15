d669cc388f8c5da15c067baf9ff2c3338be14e86
tenyaevavarvara <varvara.tenyaeva@gmail.com>
2023-11-16 02:31:22 +0300

    tenyaeva sha-2

---
+++ Heshirovanie_main/tenyaeva.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "sha2.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    string input = "Bitcoin";
+    string output1 = sha224(input);
+    string output2 = sha256(input);
+    string output3 = sha384(input);
+    string output4 = sha512(input);
+
+    cout << "sha224('" << input << "'):" << output1 << endl;
+    cout << "sha256('" << input << "'):" << output2 << endl;
+    cout << "sha384('" << input << "'):" << output3 << endl;
+    cout << "sha512('" << input << "'):" << output4 << endl;
+    return 0;
+}
