2126c2cb74389dbfb1a56b59862bdfee4121209a
gleb13371488 <gleb.vavilov.2003@mail.ru>
2023-11-24 01:13:32 +0300

    norm/strem?

--- Heshirovanie_main/Vavilov-GOST3311-94.cpp
+++ Heshirovanie_main/Vavilov-GOST3311-94.cpp
@@ -1,90 +1,114 @@
 #include <iostream>
 #include <iomanip>
+#include <cstdint>
 #include <cstring>
+#include <vector>
 
-// Функция для выполнения преобразования по таблице замен
-void substitutionTransform(const uint8_t* input, uint8_t* output) {
-    static const uint8_t sBox[8][16] = {
-        {4, 10, 9, 2, 13, 8, 0, 14, 6, 11, 1, 12, 7, 15, 5, 3},
-        {14, 11, 4, 12, 6, 13, 15, 10, 2, 3, 8, 1, 0, 7, 5, 9},
-        {5, 8, 1, 13, 10, 3, 4, 2, 14, 15, 12, 7, 6, 0, 9, 11},
-        {7, 13, 10, 1, 0, 8, 9, 15, 14, 4, 6, 12, 11, 2, 5, 3},
-        {6, 12, 7, 1, 5, 15, 13, 8, 4, 10, 9, 14, 0, 3, 11, 2},
-        {4, 11, 10, 0, 7, 2, 1, 13, 3, 6, 8, 5, 9, 12, 15, 14},
-        {13, 11, 4, 1, 3, 15, 5, 9, 0, 10, 14, 7, 6, 8, 2, 12},
-        {1, 15, 13, 0, 5, 7, 10, 4, 9, 2, 3, 14, 6, 11, 8, 12}
-    };
+// Официальная таблица замен (S-блок) для ГОСТ 34.11-94
+static const uint8_t Sbox[256] = {
+    0x4, 0xA, 0x9, 0x2, 0xD, 0x8, 0x0, 0xE,
+    0x6, 0xB, 0x1, 0xC, 0x7, 0xF, 0x5, 0x3,
+    0xE, 0xB, 0x4, 0xD, 0x1, 0xA, 0x7, 0xC,
+    0x5, 0x8, 0x3, 0xF, 0x9, 0x6, 0x0, 0x2,
+    0x4, 0xA, 0x7, 0x0, 0x1, 0x3, 0x9, 0xC,
+    0xE, 0x2, 0xB, 0xF, 0x8, 0x5, 0x6, 0xD,
+    0xB, 0x8, 0xC, 0x7, 0x1, 0xE, 0x2, 0xD,
+    0x6, 0xF, 0x0, 0x9, 0xA, 0x4, 0x5, 0x3,
+    0xF, 0x2, 0xC, 0x7, 0xA, 0xD, 0x1, 0xE,
+    0x0, 0xB, 0x9, 0x6, 0x4, 0x3, 0x5, 0x8,
+    0x9, 0xE, 0xB, 0x7, 0x2, 0x0, 0x5, 0xA,
+    0xC, 0x1, 0x4, 0x3, 0xD, 0x6, 0x8, 0xF,
+    0x0, 0xE, 0xC, 0x2, 0xB, 0x1, 0x7, 0xD,
+    0x8, 0xF, 0x5, 0xA, 0x6, 0x4, 0x9, 0x3,
+    0xE, 0x7, 0x2, 0xD, 0x1, 0xB, 0x4, 0x8,
+    0xC, 0x5, 0x6, 0x0, 0x9, 0xA, 0x3, 0xF
+};
 
-    for (int i = 0; i < 32; i += 4) {
-        uint8_t row = (input[i] << 1) | (input[i + 1] << 0);
-        uint8_t col = (input[i + 2] << 3) | (input[i + 3] << 2) | (input[i + 3] << 1) | (input[i + 3] << 0);
 
-        uint8_t value = sBox[row][col];
+constexpr uint32_t A = 0x8E20FAA7;
+constexpr uint32_t B = 0x9B05688C;
+constexpr uint32_t C = 0x7EEC6A91;
+constexpr uint32_t D = 0x2AAB10B6;
+constexpr uint32_t E = 0x8E20FAA7;
+constexpr uint32_t F = 0x9B05688C;
+constexpr uint32_t G = 0x7EEC6A91;
+constexpr uint32_t H = 0x2AAB10B6;
 
-        output[i / 2] = (value >> 3) & 0x1;
-        output[i / 2 + 16] = (value >> 2) & 0x1;
-        output[i / 2 + 32] = (value >> 1) & 0x1;
-        output[i / 2 + 48] = value & 0x1;
-    }
+// Вспомогательные функции
+uint32_t getWord(const char* data) {
+    return static_cast<uint32_t>((static_cast<uint8_t>(data[0]) << 24) |
+        (static_cast<uint8_t>(data[1]) << 16) |
+        (static_cast<uint8_t>(data[2]) << 8) |
+        static_cast<uint8_t>(data[3]));
 }
 
-// Функция для выполнения перестановок
-void permutationTransform(const uint8_t* input, uint8_t* output) {
-    static const int permutationTable[64] = {
-        40, 8, 48, 16, 56, 24, 64, 32,
-        39, 7, 47, 15, 55, 23, 63, 31,
-        38, 6, 46, 14, 54, 22, 62, 30,
-        37, 5, 45, 13, 53, 21, 61, 29,
-        36, 4, 44, 12, 52, 20, 60, 28,
-        35, 3, 43, 11, 51, 19, 59, 27,
-        34, 2, 42, 10, 50, 18, 58, 26,
-        33, 1, 41, 9, 49, 17, 57, 25
-    };
-
-    for (int i = 0; i < 64; ++i) {
-        output[i] = input[permutationTable[i] - 1];
-    }
+void putWord(char* data, uint32_t word) {
+    data[0] = static_cast<char>((word >> 24) & 0xFF);
+    data[1] = static_cast<char>((word >> 16) & 0xFF);
+    data[2] = static_cast<char>((word >> 8) & 0xFF);
+    data[3] = static_cast<char>(word & 0xFF);
 }
 
-// Функция для выполнения одного раунда хэширования
-void hashRound(const uint8_t* input, uint8_t* output) {
-    uint8_t substituted[64];
-    substitutionTransform(input, substituted);
-
-    uint8_t permuted[64];
-    permutationTransform(substituted, permuted);
-
-    memcpy(output, permuted, 64);
+// Функция подстановки
+uint32_t P(uint32_t x) {
+    return (Sbox[static_cast<uint8_t>(x >> 24)] << 24) |
+        (Sbox[static_cast<uint8_t>(x >> 16)] << 16) |
+        (Sbox[static_cast<uint8_t>(x >> 8)] << 8) |
+        Sbox[static_cast<uint8_t>(x)];
 }
 
-// Функция для выполнения полного хэширования
-void gostHash(const std::string& input, uint8_t* output) {
-    // Инициализация начального блока данных
-    uint8_t block[64];
-    memset(block, 0, sizeof(block));
-    memcpy(block, input.c_str(), std::min(input.size(), static_cast<size_t>(64)));
+// Функция шифрования одного блока
+void gostBlockEncrypt(const char* block, char* result) {
+    uint32_t N1 = getWord(block);
+    uint32_t N2 = getWord(block + 4);
 
-    // Выполнение 32 раундов хэширования
-    for (int i = 0; i < 32; ++i) {
-        hashRound(block, block);
+    for (int i = 0; i < 3; ++i) {
+        N2 ^= P(N1 + A);
+        N1 ^= P(N2 + B);
+        N2 ^= P(N1 + C);
+        N1 ^= P(N2 + D);
+        N2 ^= P(N1 + E);
+        N1 ^= P(N2 + F);
+        N2 ^= P(N1 + G);
+        N1 ^= P(N2 + H);
     }
 
-    // Результирующий хэш
-    memcpy(output, block, 32);
+    N2 ^= P(N1 + A);
+    N1 ^= P(N2 + B);
+
+    putWord(result, N2);
+    putWord(result + 4, N1);
 }
 
-int print() {
-    std::string input = "bitcoin";
-    uint8_t hash[32];
+// Хеширование
+std::string gostHash(const std::string& input) {
+    // Инициализация начального вектора
+    uint32_t h[2] = { 0, 0 };
 
-    gostHash(input, hash);
+    // Дополнение сообщения нулями
+    size_t paddedSize = (input.size() + 7) & ~0x7;
+    char* paddedData = new char[paddedSize];
+    std::memcpy(paddedData, input.c_str(), input.size());
+    std::memset(paddedData + input.size(), 0, paddedSize - input.size());
 
-    std::cout << "Input: " << input << std::endl;
-    std::cout << "Hash: ";
-    for (int i = 0; i < 32; ++i) {
-        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
+    // Обработка блоков
+    for (size_t i = 0; i < paddedSize; i += 8) {
+        char block[8];
+        std::memcpy(block, paddedData + i, 8);
+
+        // Обновление вектора хеша
+        gostBlockEncrypt(block, reinterpret_cast<char*>(h));
     }
-    std::cout << std::endl;
 
-    return 0;
+    delete[] paddedData;
+
+    // Преобразование хеша в строку
+    std::string result;
+    for (int i = 0; i < 2; ++i) {
+        char buffer[9];
+        std::sprintf(buffer, "%08x", h[i]);
+        result += buffer;
+    }
+
+    return result;
 }
